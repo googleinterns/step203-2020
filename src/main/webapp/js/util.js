dd1b22007cc282bcb4d251106883ab059b029d30
jyx11011 <jyx11011@gmail.com>
2020-06-29 10:31:54 +0800

    Modify util.js

--- src/main/webapp/js/util.js
+++ src/main/webapp/js/util.js
@@ -15,23 +15,26 @@ function addLoadEvent(func) {
   }
 }
 
+/**
+ * Configures login status in header.
+ */
 function configureHeaderLoginStatus() {
-	fetch("/api/authentication")
-	  .then(response => response.json())
-	  .then(loginStatus => {
-		const loginStatusLink = document.getElementById('login-status-link');
-		const headerUsername = document.getElementById('header-username');
-		if (loginStatus.isLoggedIn) {
-		  loginStatusLink.href = loginStatus.logoutUrl;
-		  loginStatusLink.innerText = 'Logout';
-		  headerUsername.innerText = loginStatus.userEmail;
-		  headerUsername.href = '/profile';
-		} else {
-		  loginStatusLink.href = loginStatus.loginUrl;
-		  loginStatusLink.innerText = 'Login';
-		  headerUsername.innerText = '';
-		}
-	  });
+  fetch('/api/authentication')
+      .then((response) => response.json())
+      .then((loginStatus) => {
+        const loginStatusLink = document.getElementById('login-status-link');
+        const headerUsername = document.getElementById('header-username');
+        if (loginStatus.isLoggedIn) {
+          loginStatusLink.href = loginStatus.logoutUrl;
+          loginStatusLink.innerText = 'Logout';
+          headerUsername.innerText = loginStatus.userEmail;
+          headerUsername.href = '/user';
+        } else {
+          loginStatusLink.href = loginStatus.loginUrl;
+          loginStatusLink.innerText = 'Login';
+          headerUsername.innerText = '';
+        }
+      });
 }
-  
+
 addLoadEvent(configureHeaderLoginStatus);
