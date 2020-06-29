2c7bbc2dd73761ab5c7c20ad77b98380fb6309aa
Lim Li <limlirandom@gmail.com>
2020-06-29 14:26:03 +0800

    remove post method

--- src/main/java/com/google/step/servlets/DealDetailServlet.java
+++ src/main/java/com/google/step/servlets/DealDetailServlet.java
@@ -52,38 +52,4 @@ public class DealDetailServlet extends HttpServlet {
     response.setContentType("application/json;");
     response.getWriter().println(JsonFormatter.getDealJson(deal));
   }
-
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // TODO: check user authentication
-    long id;
-    try {
-      id = Long.parseLong(request.getPathInfo().substring(1));
-    } catch (NumberFormatException e) {
-      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      return;
-    }
-    System.out.println("des: " + request.getParameter("description"));
-    System.out.println("start: " + request.getParameter("start"));
-    if (true) return;
-    String description = request.getParameter("description");
-    String photoBlobkey = null; // TODO connect to blobstore
-    String start = request.getParameter("start");
-    String end = request.getParameter("end");
-    String source = request.getParameter("source");
-    long poster = -1;
-    long restaurant = -1;
-    if (request.getParameter("restaurant") != null) {
-      try {
-        restaurant = Long.parseLong(request.getParameter("restaurant"));
-      } catch (NumberFormatException e) {
-        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        return;
-      }
-    }
-
-    Deal deal = new Deal(id, description, photoBlobkey, start, end, source, poster, restaurant);
-    manager.updateDeal(deal);
-    response.setStatus(HttpServletResponse.SC_OK);
-  }
 }
