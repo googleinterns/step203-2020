30144a5cfb4eca83dc4c6abfd67aa39254c08f56
Lim Li <limlirandom@gmail.com>
2020-06-30 13:53:25 +0800

    remove deal from tag

--- src/main/java/com/google/step/model/Tag.java
+++ src/main/java/com/google/step/model/Tag.java
@@ -3,11 +3,9 @@ package com.google.step.model;
 public class Tag {
   public final long id;
   public final String tag;
-  public final long deal;
 
-  public Tag(long id, String tag, long deal) {
+  public Tag(long id, String tag) {
     this.id = id;
     this.tag = tag;
-    this.deal = deal;
   }
 }
