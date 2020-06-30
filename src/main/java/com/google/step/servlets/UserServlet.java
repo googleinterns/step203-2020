c536499105d3ee99e3cf7bb51381925e515a70ab
jyx11011 <jyx11011@gmail.com>
2020-06-30 14:50:03 +0800

    Uses json formatter in user servlet

--- src/main/java/com/google/step/servlets/UserServlet.java
+++ src/main/java/com/google/step/servlets/UserServlet.java
@@ -2,16 +2,21 @@ package com.google.step.servlets;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-import com.google.gson.Gson;
 import com.google.step.datamanager.UserManager;
 import com.google.step.datamanager.UserManagerDatastore;
+import com.google.step.model.Deal;
+import com.google.step.model.Restaurant;
+import com.google.step.model.Tag;
 import com.google.step.model.User;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/** Servlet that handles users. */
 @WebServlet("/api/users/*")
 public class UserServlet extends HttpServlet {
 
@@ -23,10 +28,15 @@ public class UserServlet extends HttpServlet {
 
     String idString = request.getPathInfo().substring(1); // Remove '/'
     long id = Long.parseLong(idString);
-    // TODO: user json formatter
     User user = userManager.readUser(id);
-    Gson gson = new Gson();
-    String json = gson.toJson(user);
+    List<Deal> deals = new ArrayList<>(); // dealManager.getDealsPublishedByUser(id);
+    List<User> following = new ArrayList<>(); // followManager.getUsersFollowedByUser(id);
+    List<User> followers = new ArrayList<>(); // followManager.getUsersFollowingUser(id);
+    List<Tag> tags = new ArrayList<>(); // followManager.getTagsFollowedByUser(id);
+    List<Restaurant> restaurants =
+        new ArrayList<>(); // followManager.getRestaurantsFollowedByUser(id);
+
+    String json = JsonFormatter.getUserJson(user, deals, following, followers, tags, restaurants);
     response.getWriter().println(json);
   }
 
