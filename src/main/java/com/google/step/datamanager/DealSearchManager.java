834894b2407f94d0bf7df120a22097d593dd1bae
Lim Li <limlirandom@gmail.com>
2020-07-01 16:36:22 +0800

    add javadocs

--- src/main/java/com/google/step/datamanager/DealSearchManager.java
+++ src/main/java/com/google/step/datamanager/DealSearchManager.java
@@ -5,9 +5,20 @@ import java.util.List;
 
 public interface DealSearchManager {
 
+  /**
+   * Search for Deal which has all the words in query and has all the tags given.
+   *
+   * @return list of Deal IDs
+   */
   public List<Long> search(String query, List<Long> tagIds);
 
+  /** Adds a Deal to the search index */
   public void addDeal(Deal deal, List<Long> tagIds);
 
+  /**
+   * Removes a Deal from the search index
+   *
+   * @param id Deal's ID
+   */
   public void removeDeal(long id);
 }
