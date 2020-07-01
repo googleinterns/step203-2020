84c12bd3f8470ec5e0f1b60a043ae7e40d591e86
Lim Li <limlirandom@gmail.com>
2020-07-01 16:55:52 +0800

    add javadocs

--- src/main/java/com/google/step/datamanager/VoteManager.java
+++ src/main/java/com/google/step/datamanager/VoteManager.java
@@ -1,7 +1,11 @@
 package com.google.step.datamanager;
 
 public interface VoteManager {
+  /** Gets the number of votes the Deal with the given dealId has */
   public int getVotes(long dealId);
 
+  /**
+   * The user with the given userId votes on the Deal with the given dealId in the given direction.
+   */
   public void vote(long userId, long dealId, int dir);
 }
