ab169d801df40f5f3a00df40db5b159147323e18
Lisa <lisa.shannon.goh@gmail.com>
2020-07-01 22:36:02 +0800

    Add get and delete methods for servlet

---
+++ src/main/java/com/google/step/servlets/CommentServlet.java
@@ -0,0 +1,54 @@
+package com.google.step.servlets;
+
+import com.google.step.datamanager.CommentManager;
+import com.google.step.datamanager.CommentManagerDatastore;
+import com.google.step.model.Comment;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that handles individual deals. */
+@WebServlet("/api/comments/*")
+public class CommentServlet extends HttpServlet {
+
+  private final CommentManager manager;
+
+  public CommentServlet() {
+    manager = new CommentManagerDatastore();
+  }
+
+  /** Deletes the comment with the given id parameter */
+  @Override
+  public void doDelete(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    long id;
+    try {
+      id = Long.parseLong(request.getPathInfo().substring(1));
+    } catch (NumberFormatException e) {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      return;
+    }
+    manager.deleteComment(id);
+  }
+
+  /** Gets the comments for the deal with the given id parameter */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    long dealId;
+    try {
+      dealId = Long.parseLong(request.getPathInfo().substring(1));
+    } catch (NumberFormatException e) {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      return;
+    }
+    List<Comment> comments = manager.getComments(dealId);
+    if (comments == null) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      return;
+    }
+    response.setContentType("application/json;");
+    response.getWriter().println(JsonFormatter.getCommentsJson(comments));
+  }
+}
