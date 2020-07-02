db27c57a202c7551c9d17bca40eda94a7be82a53
jyx11011 <jyx11011@gmail.com>
2020-07-02 14:33:28 +0800

    Add deal tag manager

---
+++ src/main/java/com/google/step/datamanager/DealTagManager.java
@@ -0,0 +1,22 @@
+package com.google.step.datamanager;
+
+import java.util.List;
+
+/** DealTagManager handles deal tag relations. */
+public interface DealTagManager {
+  /**
+   * Returns tag ids of the deal with the id.
+   *
+   * @param id id of the deal.
+   * @return tag ids of the deal with the id.
+   */
+  public List<Long> getTagIdsOfDeal(long id);
+
+  /**
+   * Updates tags of deal with the id to be the list of tags.
+   *
+   * @param id id of the deal.
+   * @param tagIds updated tag ids of the deal.
+   */
+  public void updateTagsOfDeal(long id, List<Long> tagIds);
+}
---
+++ src/main/java/com/google/step/datamanager/DealTagManagerDatastore.java
@@ -0,0 +1,75 @@
+package com.google.step.datamanager;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class DealTagManagerDatastore implements DealTagManager {
+  private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+  @Override
+  public List<Long> getTagIdsOfDeal(long id) {
+    Iterable<Entity> results = getDealTagEntitiesOfDeal(id);
+    List<Long> tagIds = new ArrayList<>();
+    for (Entity entity : results) {
+      long tagId = (long) entity.getProperty("tagId");
+      tagIds.add(tagId);
+    }
+    return tagIds;
+  }
+
+  @Override
+  public void updateTagsOfDeal(long id, List<Long> tagIds) {
+    HashSet<Long> newTagIds = new HashSet(tagIds);
+    Iterable<Entity> results = getDealTagEntitiesOfDeal(id);
+
+    // Keeps tags in the new list and remove others
+    for (Entity entity : results) {
+      long tagId = (long) entity.getProperty("tagId");
+      if (newTagIds.contains(tagId)) {
+        newTagIds.remove(tagId);
+        continue;
+      }
+      datastore.delete(entity.getKey());
+    }
+
+    // Adds remaining tags in the new list
+    for (long tagId : newTagIds) {
+      Entity entity = createDealTagEntity(id, tagId);
+      datastore.put(entity);
+    }
+  }
+
+  /**
+   * Returns an iterable list of deal-tag entities with the deal id.
+   *
+   * @param dealId id of the deal.
+   * @return an iterable list of deal-tag entities with the deal id.
+   */
+  private Iterable<Entity> getDealTagEntitiesOfDeal(long dealId) {
+    Query query =
+        new Query("DealTag")
+            .setFilter(new Query.FilterPredicate("dealId", Query.FilterOperator.EQUAL, dealId));
+
+    Iterable<Entity> results = datastore.prepare(query).asIterable();
+    return results;
+  }
+
+  /**
+   * Returns a deal tag entity with the given deal id and tag id.
+   *
+   * @param dealId id of the deal.
+   * @param tagId id of the tag.
+   * @return a deal tag entity with the given deal id and tag id.
+   */
+  private Entity createDealTagEntity(long dealId, long tagId) {
+    Entity entity = new Entity("DealTag");
+    entity.setProperty("dealId", dealId);
+    entity.setProperty("tagId", tagId);
+    return entity;
+  }
+}
