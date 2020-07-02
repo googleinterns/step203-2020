eb28d513d9e92bb8bba2df5933a78ca637037249
Lim Li <limlirandom@gmail.com>
2020-07-02 16:22:59 +0800

    consider corner case for wrong date format

--- src/main/java/com/google/step/servlets/DealPostServlet.java
+++ src/main/java/com/google/step/servlets/DealPostServlet.java
@@ -4,8 +4,8 @@ import com.google.step.datamanager.DealManager;
 import com.google.step.datamanager.DealManagerDatastore;
 import com.google.step.model.Deal;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -73,10 +73,10 @@ public class DealPostServlet extends HttpServlet {
   }
 
   private boolean isValidDate(String date) {
-    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     try {
-      format.parse(date);
-    } catch (ParseException e) {
+      formatter.parse(date);
+    } catch (DateTimeParseException e) {
       return false;
     }
     return true;
