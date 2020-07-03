589d64770e7cc17a23c47df3962651daad6d0e33
Lisa <lisa.shannon.goh@gmail.com>
2020-07-03 18:16:34 +0800

    Refractor constants to uppercase

--- src/test/java/com/google/step/datamanager/CommentManagerDatastoreTest.java
+++ src/test/java/com/google/step/datamanager/CommentManagerDatastoreTest.java
@@ -18,15 +18,13 @@ import java.util.ArrayList;
 @RunWith(JUnit4.class)
 public final class CommentManagerDatastoreTest {
 
-  private static final long dealId = 2;
+  private static final long DEALID = 2;
 
-  private static final long ID_A = 1;
-  private static final long userId_A = 3;
-  private static final String content_A = "Hello world";
+  private static final long USERID_A = 3;
+  private static final String CONTENT_A = "Hello world";
 
-  private static final long ID_B = 2;
-  private static final long userId_B = 4;
-  private static final String content_B = "Hello world2";
+  private static final long USERID_B = 4;
+  private static final String CONTENT_B = "Hello world2";
 
   private final LocalServiceTestHelper helper =
       new LocalServiceTestHelper(
@@ -46,64 +44,64 @@ public final class CommentManagerDatastoreTest {
 
   @Test
   public void testCreateComment() {
-    Comment comment = commentManagerDatastore.createComment(dealId, userId_A, content_A);
-    Assert.assertEquals(dealId, comment.dealId);
-    Assert.assertEquals(userId_A, comment.userId);
-    Assert.assertEquals(content_A, comment.content);
+    Comment comment = commentManagerDatastore.createComment(DEALID, USERID_A, CONTENT_A);
+    Assert.assertEquals(DEALID, comment.dealId);
+    Assert.assertEquals(USERID_A, comment.userId);
+    Assert.assertEquals(CONTENT_A, comment.content);
   }
 
   @Test
   public void testGetComments() {
-    Comment comment_A = commentManagerDatastore.createComment(dealId, userId_A, content_A);
-    Comment comment_B = commentManagerDatastore.createComment(dealId, userId_B, content_B);
-    List<Comment> comments = commentManagerDatastore.getComments(dealId);
+    Comment comment_A = commentManagerDatastore.createComment(DEALID, USERID_A, CONTENT_A);
+    Comment comment_B = commentManagerDatastore.createComment(DEALID, USERID_B, CONTENT_B);
+    List<Comment> comments = commentManagerDatastore.getComments(DEALID);
     Comment comment_A_Test = comments.get(0);
     Comment comment_B_Test = comments.get(1);
-    assertEquals(dealId, comment_A_Test.dealId);
-    assertEquals(userId_A, comment_A_Test.userId);
-    assertEquals(content_A, comment_A_Test.content);
-    assertEquals(dealId, comment_B_Test.dealId);
-    assertEquals(userId_B, comment_B_Test.userId);
-    assertEquals(content_B, comment_B_Test.content);
+    assertEquals(DEALID, comment_A_Test.dealId);
+    assertEquals(USERID_A, comment_A_Test.userId);
+    assertEquals(CONTENT_A, comment_A_Test.content);
+    assertEquals(DEALID, comment_B_Test.dealId);
+    assertEquals(USERID_B, comment_B_Test.userId);
+    assertEquals(CONTENT_B, comment_B_Test.content);
   }
 
   @Test
   public void testDeleteSingleComment() {
-    Comment comment_A = commentManagerDatastore.createComment(dealId, userId_A, content_A);
+    Comment comment_A = commentManagerDatastore.createComment(DEALID, USERID_A, CONTENT_A);
     commentManagerDatastore.deleteComment(comment_A.id);
-    Assert.assertTrue(commentManagerDatastore.getComments(dealId).isEmpty());;
+    Assert.assertTrue(commentManagerDatastore.getComments(DEALID).isEmpty());
   }
 
   @Test
   public void testDeleteBothComments() {
-    Comment comment_A = commentManagerDatastore.createComment(dealId, userId_A, content_A);
-    Comment comment_B = commentManagerDatastore.createComment(dealId, userId_B, content_B);
+    Comment comment_A = commentManagerDatastore.createComment(DEALID, USERID_A, CONTENT_A);
+    Comment comment_B = commentManagerDatastore.createComment(DEALID, USERID_B, CONTENT_B);
     commentManagerDatastore.deleteComment(comment_A.id);
     commentManagerDatastore.deleteComment(comment_B.id);
-    Assert.assertTrue(commentManagerDatastore.getComments(dealId).isEmpty());
+    Assert.assertTrue(commentManagerDatastore.getComments(DEALID).isEmpty());
   }
 
   @Test
   public void testDeleteOnlyOneComment() {
-    Comment comment_A = commentManagerDatastore.createComment(dealId, userId_A, content_A);
-    Comment comment_B = commentManagerDatastore.createComment(dealId, userId_B, content_B);
+    Comment comment_A = commentManagerDatastore.createComment(DEALID, USERID_A, CONTENT_A);
+    Comment comment_B = commentManagerDatastore.createComment(DEALID, USERID_B, CONTENT_B);
     commentManagerDatastore.deleteComment(comment_A.id);
-    List<Comment> comments = commentManagerDatastore.getComments(dealId);
+    List<Comment> comments = commentManagerDatastore.getComments(DEALID);
     Comment comment_B_Test = comments.get(0);
-    Assert.assertEquals(dealId, comment_B_Test.dealId);
-    Assert.assertEquals(userId_B, comment_B_Test.userId);
-    Assert.assertEquals(content_B, comment_B_Test.content);
+    Assert.assertEquals(DEALID, comment_B_Test.dealId);
+    Assert.assertEquals(USERID_B, comment_B_Test.userId);
+    Assert.assertEquals(CONTENT_B, comment_B_Test.content);
     Assert.assertEquals(1, comments.size());
   }
 
   @Test
   public void testUpdateComment() {
-    Comment comment_A = commentManagerDatastore.createComment(dealId, userId_A, content_A);
+    Comment comment_A = commentManagerDatastore.createComment(DEALID, USERID_A, CONTENT_A);
     Comment comment_A_Updated = commentManagerDatastore.updateComment(comment_A.id, "Updated comment");
-    List<Comment> comments = commentManagerDatastore.getComments(dealId);
+    List<Comment> comments = commentManagerDatastore.getComments(DEALID);
     Comment comment_A_Test = comments.get(0);
-    Assert.assertEquals(dealId, comment_A_Test.dealId);
-    Assert.assertEquals(userId_A, comment_A_Test.userId);
+    Assert.assertEquals(DEALID, comment_A_Test.dealId);
+    Assert.assertEquals(USERID_A, comment_A_Test.userId);
     Assert.assertEquals("Updated comment", comment_A_Test.content);
   }
 }
--- src/test/java/com/google/step/servlets/CommentServletTest.java
+++ src/test/java/com/google/step/servlets/CommentServletTest.java
@@ -22,17 +22,17 @@ import java.util.ArrayList;
 @RunWith(JUnit4.class)
 public class CommentServletTest {
 
-    private static final long dealId = 2;
+    private static final long DEALID = 2;
 
     private static final long ID_A = 1;
-    private static final long userId_A = 3;
-    private static final String content_A = "Hello world";
-    private static final Comment comment_A = new Comment(ID_A, dealId, userId_A, content_A);
+    private static final long USERID_A = 3;
+    private static final String CONTENT_A = "Hello world";
+    private static final Comment COMMENT_A = new Comment(ID_A, DEALID, USERID_A, CONTENT_A);
 
     private static final long ID_B = 2;
     private static final long userId_B = 4;
-    private static final String content_B = "Hello world2";
-    private static final Comment comment_B = new Comment(ID_B, dealId, userId_B, content_B);
+    private static final String CONTENT_B = "Hello world2";
+    private static final Comment COMMENT_B = new Comment(ID_B, DEALID, userId_B, CONTENT_B);
 
     private CommentManager commentManager;
 
@@ -50,8 +50,8 @@ public class CommentServletTest {
     @Test
     public void testDoGet_success() throws Exception {
         List<Comment> comments = new ArrayList<>();
-        comments.add(comment_A);
-        comments.add(comment_B);
+        comments.add(COMMENT_A);
+        comments.add(COMMENT_B);
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
 
@@ -65,9 +65,9 @@ public class CommentServletTest {
         commentServlet.doGet(request, response);
 
         String commentA = String.format("{id:%d,dealId:%d,userId:%d,content:\"%s\"}",
-                          ID_A, dealId, userId_A, content_A);
+                          ID_A, DEALID, USERID_A, CONTENT_A);
         String commentB = String.format("{id:%d,dealId:%d,userId:%d,content:\"%s\"}",
-                          ID_B, dealId, userId_B, content_B);
+                          ID_B, DEALID, userId_B, CONTENT_B);
         String expected = "[" + commentA + "," + commentB + "]";
 
         JSONAssert.assertEquals(expected, stringWriter.toString(), JSONCompareMode.STRICT);
@@ -111,13 +111,13 @@ public class CommentServletTest {
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
 
-        when(request.getParameter("dealId")).thenReturn(Long.toString(dealId));
-        when(request.getParameter("userId")).thenReturn(Long.toString(userId_A));
-        when(request.getParameter("content")).thenReturn(content_A);
-        when(commentManager.createComment(dealId, userId_A, content_A)).thenReturn(comment_A);
+        when(request.getParameter("dealId")).thenReturn(Long.toString(DEALID));
+        when(request.getParameter("userId")).thenReturn(Long.toString(USERID_A));
+        when(request.getParameter("content")).thenReturn(CONTENT_A);
+        when(commentManager.createComment(DEALID, USERID_A, CONTENT_A)).thenReturn(COMMENT_A);
         commentServletPost.doPost(request, response);
 
-        comments.add(comment_A);
+        comments.add(COMMENT_A);
         when(request.getPathInfo()).thenReturn("/2");
         when(commentManager.getComments(2)).thenReturn(comments);
 
@@ -128,7 +128,7 @@ public class CommentServletTest {
         commentServlet.doGet(request, response);
 
         String expected = String.format("[{id:%d,dealId:%d,userId:%d,content:\"%s\"}]",
-                          ID_A, dealId, userId_A, content_A);
+                          ID_A, DEALID, USERID_A, CONTENT_A);
 
         System.out.println(stringWriter.toString());
 
