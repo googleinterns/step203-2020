188bc5fbc0e829046fa38a9a2929bdabb4606b4a
Lisa <lisa.shannon.goh@gmail.com>
2020-07-06 16:48:03 +0800

    Fix typo and checking of null

--- src/main/java/com/google/step/datamanager/CommentManagerDatastore.java
+++ src/main/java/com/google/step/datamanager/CommentManagerDatastore.java
@@ -12,11 +12,11 @@ import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.step.model.Comment;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CommentManagerDatastore implements CommentManager {
-    
+
   private final DatastoreService datastore;
 
   public CommentManagerDatastore() {
@@ -24,10 +24,7 @@ public class CommentManagerDatastore implements CommentManager {
   }
 
   @Override
-  public Comment createComment(
-    long dealId, 
-    long userId, 
-    String content) {
+  public Comment createComment(long dealId, long userId, String content) {
     Entity entity = new Entity("Comment");
     entity.setProperty("deal", dealId);
     entity.setProperty("user", userId);
@@ -50,21 +47,21 @@ public class CommentManagerDatastore implements CommentManager {
     List<Comment> comments = new ArrayList<>();
     for (Entity entity : pq.asIterable()) {
       long id = entity.getKey().getId();
-      long userId = (long) entity.getProperty("user"); 
+      long userId = (long) entity.getProperty("user");
       String content = (String) entity.getProperty("content");
       comments.add(new Comment(id, dealId, userId, content));
     }
     return comments;
   }
 
-  /**Deletes the comment with the given commentId */
+  /** Deletes the comment with the given commentId */
   @Override
   public void deleteComment(long id) {
     Key key = KeyFactory.createKey("Comment", id);
     datastore.delete(key);
   }
 
-  /**Updates the comment with the given commentId */
+  /** Updates the comment with the given commentId */
   @Override
   public Comment updateComment(long id, String content) {
     Key key = KeyFactory.createKey("Comment", id);
@@ -74,13 +71,13 @@ public class CommentManagerDatastore implements CommentManager {
     } catch (EntityNotFoundException e) {
       return null;
     }
-    if (content == null) {
-      return;
+    if (content != null) {
+      commentEntity.setProperty("content", content);
     }
-    commentEntity.setProperty("content", content
     datastore.put(commentEntity);
     long dealId = (long) commentEntity.getProperty("deal");
     long userId = (long) commentEntity.getProperty("user");
+    content = (String) commentEntity.getProperty("content");
     return new Comment(id, dealId, userId, content);
   }
 }
