8babe915371a1faedeb6922921dbf37af9da53c7
jyx11011 <jyx11011@gmail.com>
2020-07-07 17:22:52 +0800

    Add user post url servlet

---
+++ src/main/java/com/google/step/servlets/UserPostUrlServlet.java
@@ -0,0 +1,18 @@
+package com.google.step.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that handles users. */
+@WebServlet("/api/users/*")
+public class UserPostUrlServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html");
+    response.getWriter().println(ImageUploader.getUploadUrl("/api/users"));
+  }
+}
