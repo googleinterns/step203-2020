0d66851890c5afbe0346eaf6fab9ddff3fde6f5d
Lisa <lisa.shannon.goh@gmail.com>
2020-07-08 20:28:09 +0800

    Add getdealpublishedby methods in dealmanager interface

--- src/main/java/com/google/step/datamanager/DealManager.java
+++ src/main/java/com/google/step/datamanager/DealManager.java
@@ -1,6 +1,7 @@
 package com.google.step.datamanager;
 
 import com.google.step.model.Deal;
+import java.util.List;
 
 public interface DealManager {
   public Deal createDeal(
@@ -17,4 +18,15 @@ public interface DealManager {
   public Deal updateDeal(Deal deal);
 
   public void deleteDeal(long id);
+
+  public List<Deal> getTrendingDeals();
+
+  public List<Deal> getDealPublishedByFollowedRestaurantsOrUsers(
+      long userId, String fieldName, String filterAttribute);
+
+  public List<Deal> getDealsPublishedByFollowedUsers(long userId);
+
+  public List<Deal> getDealsPublishedByFollowedRestaurants(long userId);
+
+  public List<Deal> getDealsPublishedByFollowedTags(long userId);
 }
