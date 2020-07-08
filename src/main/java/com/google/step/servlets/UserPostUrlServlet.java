8a1d31ad3ecf3a1922cd49dfc7a8651bbc3825f7
jyx11011 <jyx11011@gmail.com>
2020-07-08 16:18:19 +0800

    Update javadoc

--- src/main/java/com/google/step/servlets/UserPostUrlServlet.java
+++ src/main/java/com/google/step/servlets/UserPostUrlServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that handles users. */
+/** Servlet that handles usr upload url. */
 @WebServlet("/api/user-post-url")
 public class UserPostUrlServlet extends HttpServlet {
 
