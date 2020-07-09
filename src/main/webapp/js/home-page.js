1756e8f00de92d4727b17676d75634751eac5c26
Lisa <lisa.shannon.goh@gmail.com>
2020-07-09 15:45:44 +0800

    Use util js function

--- src/main/webapp/js/home-page.js
+++ src/main/webapp/js/home-page.js
@@ -60,22 +60,6 @@ const homePage = {
 };
 
 /**
- * Adds a function to window onload event.
- * @param {function} func The function to be executed.
- */
-function addLoadEvent(func) {
-  const oldonload = window.onload;
-  if (typeof oldonload == 'function') {
-    window.onload = function() {
-      oldonload();
-      func();
-    };
-  } else {
-    window.onload = func;
-  }
-}
-
-/**
  * Creates deal elements on home page
  * @param {object} homePage
  */
