1a59e98c19605956e7f4006de85f89bcbf30c147
jyx11011 <jyx11011@gmail.com>
2020-07-09 15:19:57 +0800

    Fix typo

--- src/main/java/com/google/step/servlets/UserPostUrlServlet.java
+++ src/main/java/com/google/step/servlets/UserPostUrlServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that handles usr upload url. */
+/** Servlet that handles user upload url. */
 @WebServlet("/api/user-post-url/*")
 public class UserPostUrlServlet extends HttpServlet {
 
