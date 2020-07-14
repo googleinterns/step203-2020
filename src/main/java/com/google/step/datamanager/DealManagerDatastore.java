8aecf12af5189a1d92a6db99ca26617009a6c60d
Lisa <lisa.shannon.goh@gmail.com>
2020-07-14 13:10:41 +0800

    Change to string constant

--- src/main/java/com/google/step/datamanager/DealManagerDatastore.java
+++ src/main/java/com/google/step/datamanager/DealManagerDatastore.java
@@ -16,6 +16,7 @@ public class DealManagerDatastore implements DealManager {
 
   private final DatastoreService datastore;
   private final DealSearchManager searchManager;
+  private final String LOCATION = "Asia/Singapore";
 
   public DealManagerDatastore() {
     datastore = DatastoreServiceFactory.getDatastoreService();
@@ -39,7 +40,7 @@ public class DealManagerDatastore implements DealManager {
     entity.setProperty("source", source);
     entity.setProperty("posterId", posterId);
     entity.setProperty("restaurantId", restaurantId);
-    String timestamp = LocalDateTime.now(ZoneId.of("Asia/Singapore")).toString();
+    String timestamp = LocalDateTime.now(ZoneId.of(LOCATION)).toString();
     entity.setProperty("timestamp", timestamp);
 
     Key key = datastore.put(entity);
