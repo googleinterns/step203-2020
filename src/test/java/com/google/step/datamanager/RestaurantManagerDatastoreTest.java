d3b6d0b3197ff7afe9ccd039da7f321e52fdd1e2
Lisa <lisa.shannon.goh@gmail.com>
2020-07-15 17:42:18 +0800

    Merge

---
+++ src/test/java/com/google/step/datamanager/RestaurantManagerDatastoreTest.java
@@ -0,0 +1,123 @@
+package com.google.step.datamanager;
+
+import static com.google.step.TestConstants.BLOBKEY_A;
+import static com.google.step.TestConstants.BLOBKEY_B;
+import static com.google.step.TestConstants.RESTAURANT_NAME_A;
+import static com.google.step.TestConstants.RESTAURANT_NAME_B;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.step.model.Restaurant;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class RestaurantManagerDatastoreTest {
+
+  private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(
+          new LocalDatastoreServiceTestConfig().setApplyAllHighRepJobPolicy());
+
+  private final RestaurantManagerDatastore restaurantManagerDatastore =
+      new RestaurantManagerDatastore();
+
+  @Before
+  public void setUp() {
+    helper.setUp();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
+
+  @Test
+  public void testCreateRestaurant_success() throws Exception {
+    Restaurant restaurant =
+        restaurantManagerDatastore.createRestaurant(RESTAURANT_NAME_A, BLOBKEY_A);
+
+    assertEquals(RESTAURANT_NAME_A, restaurant.name);
+    assertEquals(BLOBKEY_A, restaurant.photoBlobkey);
+  }
+
+  @Test
+  public void testReadRestaurant_success() throws Exception {
+    Restaurant restaurantA =
+        restaurantManagerDatastore.createRestaurant(RESTAURANT_NAME_A, BLOBKEY_A);
+    Restaurant restaurantA_Test = restaurantManagerDatastore.readRestaurant(restaurantA.id);
+
+    assertEquals(RESTAURANT_NAME_A, restaurantA_Test.name);
+    assertEquals(BLOBKEY_A, restaurantA_Test.photoBlobkey);
+  }
+
+  @Test
+  public void testReadRestaurant_doesNotExist() throws Exception {
+    Restaurant restaurant = restaurantManagerDatastore.readRestaurant(1000);
+
+    assertNull(restaurant);
+  }
+
+  @Test
+  public void testUpdateRestaurant_name() throws Exception {
+    Restaurant restaurantA =
+        restaurantManagerDatastore.createRestaurant(RESTAURANT_NAME_A, BLOBKEY_A);
+    Restaurant restaurantA_New = new Restaurant(restaurantA.id, RESTAURANT_NAME_B, null);
+    Restaurant restaurantA_Updated = restaurantManagerDatastore.updateRestaurant(restaurantA_New);
+
+    assertEquals(RESTAURANT_NAME_B, restaurantA_Updated.name);
+    assertEquals(BLOBKEY_A, restaurantA_Updated.photoBlobkey);
+  }
+
+  @Test
+  public void testUpdateRestaurant_blobKey() throws Exception {
+    Restaurant restaurantA =
+        restaurantManagerDatastore.createRestaurant(RESTAURANT_NAME_A, BLOBKEY_A);
+    Restaurant restaurantA_New = new Restaurant(restaurantA.id, null, BLOBKEY_B);
+    Restaurant restaurantA_Updated = restaurantManagerDatastore.updateRestaurant(restaurantA_New);
+
+    assertEquals(RESTAURANT_NAME_A, restaurantA_Updated.name);
+    assertEquals(BLOBKEY_B, restaurantA_Updated.photoBlobkey);
+  }
+
+  @Test
+  public void testDeleteRestaurant() throws Exception {
+    Restaurant restaurantA =
+        restaurantManagerDatastore.createRestaurant(RESTAURANT_NAME_A, BLOBKEY_A);
+    restaurantManagerDatastore.deleteRestaurant(restaurantA.id);
+
+    assertNull(restaurantManagerDatastore.readRestaurant(restaurantA.id));
+  }
+
+  @Test
+  public void testSearchRestaurant() {
+    Restaurant restaurantA = restaurantManagerDatastore.createRestaurant("abcde", BLOBKEY_A);
+    Restaurant restaurantB = restaurantManagerDatastore.createRestaurant("abxyz", BLOBKEY_A);
+    Restaurant restaurantC = restaurantManagerDatastore.createRestaurant("aqqq", BLOBKEY_A);
+
+    List<Restaurant> restaurants = restaurantManagerDatastore.searchRestaurants("ab");
+
+    assertEquals(2, restaurants.size());
+    assertThat(restaurants, hasItems(restaurantA, restaurantB));
+    assertThat(restaurants, not(hasItem(restaurantC)));
+  }
+
+  @Test
+  public void testSearchRestaurant_caseInsensitive() {
+    Restaurant restaurantA = restaurantManagerDatastore.createRestaurant("AbCdE", BLOBKEY_A);
+
+    List<Restaurant> restaurants = restaurantManagerDatastore.searchRestaurants("abcde");
+
+    assertEquals(1, restaurants.size());
+    assertThat(restaurants, hasItem(restaurantA));
+  }
+}
---
+++ src/test/java/com/google/step/servlets/CommentGetPostServletTest.java
@@ -0,0 +1,148 @@
+package com.google.step.servlets;
+
+import static com.google.step.TestConstants.COMMENT_ID_A;
+import static com.google.step.TestConstants.COMMENT_ID_B;
+import static com.google.step.TestConstants.CONTENT_A;
+import static com.google.step.TestConstants.CONTENT_B;
+import static com.google.step.TestConstants.DEAL_ID_A;
+import static com.google.step.TestConstants.TIME_A;
+import static com.google.step.TestConstants.TIME_B;
+import static com.google.step.TestConstants.USER_ID_A;
+import static com.google.step.TestConstants.USER_ID_B;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.step.datamanager.CommentManager;
+import com.google.step.model.Comment;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+@RunWith(JUnit4.class)
+public class CommentGetPostServletTest {
+
+  private static final Comment COMMENT_A =
+      new Comment(COMMENT_ID_A, DEAL_ID_A, USER_ID_A, CONTENT_A, TIME_A);
+
+  private static final Comment COMMENT_B =
+      new Comment(COMMENT_ID_B, DEAL_ID_A, USER_ID_B, CONTENT_B, TIME_B);
+
+  private CommentManager mockCommentManager;
+
+  private CommentGetPostServlet commentGetPostServlet;
+
+  @Before
+  public void setUp() {
+    mockCommentManager = mock(CommentManager.class);
+    commentGetPostServlet = new CommentGetPostServlet(mockCommentManager);
+  }
+
+  @Test
+  public void testDoGet_success() throws Exception {
+    List<Comment> comments = new ArrayList<>();
+    comments.add(COMMENT_A);
+    comments.add(COMMENT_B);
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+
+    when(request.getParameter("dealId")).thenReturn(Long.toString(DEAL_ID_A));
+    when(mockCommentManager.getCommentsForDeal(DEAL_ID_A)).thenReturn(comments);
+
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(response.getWriter()).thenReturn(writer);
+
+    commentGetPostServlet.doGet(request, response);
+
+    String commentA =
+        String.format(
+            "{id:%d,dealId:%d,userId:%d,content:\"%s\",timestamp:\"%s\"}",
+            COMMENT_ID_A, DEAL_ID_A, USER_ID_A, CONTENT_A, TIME_A);
+    String commentB =
+        String.format(
+            "{id:%d,dealId:%d,userId:%d,content:\"%s\",timestamp:\"%s\"}",
+            COMMENT_ID_B, DEAL_ID_A, USER_ID_B, CONTENT_B, TIME_B);
+    String expected = "[" + commentA + "," + commentB + "]";
+
+    JSONAssert.assertEquals(expected, stringWriter.toString(), JSONCompareMode.STRICT);
+  }
+
+  @Test
+  public void testDoGet_notExist() throws Exception {
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    when(request.getParameter("dealId")).thenReturn("1000");
+    when(mockCommentManager.getCommentsForDeal(1000)).thenReturn(new ArrayList<>());
+
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(response.getWriter()).thenReturn(writer);
+
+    commentGetPostServlet.doGet(request, response);
+
+    String expected = "[]";
+
+    JSONAssert.assertEquals(expected, stringWriter.toString(), JSONCompareMode.STRICT);
+  }
+
+  @Test
+  public void testDoGet_invalidID() throws Exception {
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    when(request.getParameter("deaId")).thenReturn("abcd");
+
+    commentGetPostServlet.doGet(request, response);
+
+    verify(response).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+  }
+
+  @Test
+  public void testDoGet_noId() throws Exception {
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    when(request.getParameter("dealId")).thenReturn("");
+
+    commentGetPostServlet.doGet(request, response);
+    verify(response).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+  }
+
+  @Test
+  public void testDoPost_success() throws Exception {
+    List<Comment> comments = new ArrayList<>();
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+
+    when(request.getParameter("dealId")).thenReturn(Long.toString(DEAL_ID_A));
+    when(request.getParameter("content")).thenReturn(CONTENT_A);
+    when(mockCommentManager.createComment(DEAL_ID_A, USER_ID_A, CONTENT_A)).thenReturn(COMMENT_A);
+
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(response.getWriter()).thenReturn(writer);
+
+    commentGetPostServlet.doPost(request, response);
+
+    verify(response, never()).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    verify(mockCommentManager).createComment(anyLong(), anyLong(), eq(CONTENT_A));
+
+    String expected =
+        String.format(
+            "{id:%d,dealId:%d,userId:%d,content:\"%s\",timestamp:\"%s\"}",
+            COMMENT_ID_A, DEAL_ID_A, USER_ID_A, CONTENT_A, TIME_A);
+
+    JSONAssert.assertEquals(expected, stringWriter.toString(), JSONCompareMode.STRICT);
+  }
+}
