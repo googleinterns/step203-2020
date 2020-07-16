024e8c5ac0d0411de97a5d06a75a821185229184
Lim Li <limlirandom@gmail.com>
2020-07-16 22:03:23 +0800

    update javadocs

--- src/main/java/com/google/step/servlets/RestaurantPostUrlServlet.java
+++ src/main/java/com/google/step/servlets/RestaurantPostUrlServlet.java
@@ -6,11 +6,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that gets the url for posting deals. */
+/** Servlet that gets the url for posting restaurants. */
 @WebServlet("/api/upload-restaurant-url")
 public class RestaurantPostUrlServlet extends HttpServlet {
 
-  /** Gets URL for posting deals */
+  /** Gets URL for posting restaurants */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html");
