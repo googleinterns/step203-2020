f2bd5f2ea57d37e7a95c378aa7fefbd28a84d186
jyx11011 <jyx11011@gmail.com>
2020-07-20 14:41:54 +0800

    revert changes

--- src/main/java/com/google/step/datamanager/FollowManager.java
+++ src/main/java/com/google/step/datamanager/FollowManager.java
@@ -10,8 +10,6 @@ public interface FollowManager {
 
   public Set<Long> getFollowedRestaurantIds(long followerId);
 
-  public boolean isFollowingRestaurant(long followerId, long followeeId);
-
   public void followUser(long followerId, long userId);
 
   public void unfollowUser(long followerId, long userId);
@@ -20,7 +18,7 @@ public interface FollowManager {
 
   public Set<Long> getFollowerIdsOfUser(long followeeId);
 
-  public boolean isFollowingUser(long followerId, long followeeId);
+  public boolean isFollowing(long followerId, long followeeId);
 
   public void followTag(long followerId, long tagId);
 
