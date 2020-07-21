a6b0e6c69c21a11639f40e418f6deb68f172c1cb
Lisa <lisa.shannon.goh@gmail.com>
2020-07-21 16:12:36 +0800

    Rename image to photoUrl

--- src/test/java/com/google/step/TestConstants.java
+++ src/test/java/com/google/step/TestConstants.java
@@ -125,7 +125,7 @@ public class TestConstants {
   // Deal Brief for Home Page
   public static final String HOME_DEAL_A_JSON =
       String.format(
-          "{restaurant: {id: %d, name: \"%s\", image:  \"%s\"}, description: \"%s\", votes: %d, id: %d, pic: \"%s\", poster:{id: %d, username: \"%s\", picture: \"%s\"}, tags: [{id: %d, name: \"%s\"}], timestamp: \"%s\"}",
+          "{restaurant: {id: %d, name: \"%s\", photoUrl:  \"%s\"}, description: \"%s\", votes: %d, id: %d, pic: \"%s\", poster:{id: %d, username: \"%s\", picture: \"%s\"}, tags: [{id: %d, name: \"%s\"}], timestamp: \"%s\"}",
           RESTAURANT_ID_A,
           RESTAURANT_NAME_A,
           BLOBKEY_URL_A,
