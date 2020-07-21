4f0a526a4650f11ed8907f623b70391a77c57dd9
jyx11011 <jyx11011@gmail.com>
2020-07-21 14:19:10 +0800

    Add restaurant place data manager

---
+++ src/main/java/com/google/step/datamanager/RestaurantPlaceManagerDatastore.java
@@ -0,0 +1,65 @@
+package com.google.step.datamanager;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RestaurantPlaceManagerDatastore implements RestaurantPlaceManager {
+
+  private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+  @Override
+  public void updatePlacesOfRestaurants(long restaurantId, List<String> placeIds) {
+    Set<String> newPlaceIds = new HashSet<>(placeIds);
+    Iterable<Entity> results = getEntitiesOfRestaurant(restaurantId);
+
+    // Keeps place ids in the new list and remove others
+    for (Entity entity : results) {
+      String placeId = (String) entity.getProperty("placeId");
+      if (newPlaceIds.contains(placeId)) {
+        newPlaceIds.remove(placeId);
+        continue;
+      }
+      datastore.delete(entity.getKey());
+    }
+
+    // Adds remaining tags in the new list
+    for (String placeId : newPlaceIds) {
+      Entity entity = createRestaurantPlaceEntity(restaurantId, placeId);
+      datastore.put(entity);
+    }
+  }
+
+  @Override
+  public Set<String> getPlaceIdsOfRestaurant(long id) {
+    Iterable<Entity> results = getEntitiesOfRestaurant(id);
+    Set<String> placeIds = new HashSet<>();
+    for (Entity entity : results) {
+      placeIds.add((String) entity.getProperty("placeId"));
+    }
+
+    return placeIds;
+  }
+
+  private Entity createRestaurantPlaceEntity(long restaurantId, String placeId) {
+    Entity entity = new Entity("RestaurantPlace");
+    entity.setProperty("restaurantId", restaurantId);
+    entity.setProperty("placeId", placeId);
+    return entity;
+  }
+
+  private Iterable<Entity> getEntitiesOfRestaurant(long id) {
+    Query query = new Query("RestaurantPlace");
+    Filter filter = new FilterPredicate("restaurantId", FilterOperator.EQUAL, id);
+    query.setFilter(filter);
+    Iterable<Entity> results = datastore.prepare(query).asIterable();
+    return results;
+  }
+}
---
+++ src/test/java/com/google/step/datamanager/RestaurantPlaceManagerDatastoreTest.java
@@ -0,0 +1,58 @@
+package com.google.step.datamanager;
+
+import static com.google.step.TestConstants.PLACE_ID_A;
+import static com.google.step.TestConstants.PLACE_ID_B;
+import static com.google.step.TestConstants.PLACE_ID_C;
+import static com.google.step.TestConstants.PLACE_ID_D;
+import static com.google.step.TestConstants.RESTAURANT_ID_A;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.junit.Assert.assertThat;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class RestaurantPlaceManagerDatastoreTest {
+  private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+  private final RestaurantPlaceManagerDatastore restaurantPlaceManager =
+      new RestaurantPlaceManagerDatastore();
+
+  @Before
+  public void setUp() {
+    helper.setUp();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
+
+  @Test
+  public void testUpdatePlacesOfRestaurant() {
+    List<String> placeIds = Arrays.asList(PLACE_ID_A, PLACE_ID_B);
+    restaurantPlaceManager.updatePlacesOfRestaurants(RESTAURANT_ID_A, placeIds);
+
+    Set<String> actual = restaurantPlaceManager.getPlaceIdsOfRestaurant(RESTAURANT_ID_A);
+    assertThat(actual, hasItems(PLACE_ID_A, PLACE_ID_B));
+  }
+
+  @Test
+  public void testUpdatePlacesOfRestaurant_morePlaceIds() {
+    List<String> placeIds = Arrays.asList(PLACE_ID_A, PLACE_ID_B);
+    restaurantPlaceManager.updatePlacesOfRestaurants(RESTAURANT_ID_A, placeIds);
+    placeIds = Arrays.asList(PLACE_ID_A, PLACE_ID_C, PLACE_ID_D);
+    restaurantPlaceManager.updatePlacesOfRestaurants(RESTAURANT_ID_A, placeIds);
+
+    Set<String> actual = restaurantPlaceManager.getPlaceIdsOfRestaurant(RESTAURANT_ID_A);
+    assertThat(actual, hasItems(PLACE_ID_A, PLACE_ID_C, PLACE_ID_D));
+  }
+}
