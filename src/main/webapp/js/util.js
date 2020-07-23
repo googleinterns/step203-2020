da2a14f27593d507be6df7d8206f1cc5f3648ca5
Lisa <lisa.shannon.goh@gmail.com>
2020-07-23 15:29:10 +0800

    Remove commented code

--- src/main/webapp/js/util.js
+++ src/main/webapp/js/util.js
@@ -202,33 +202,3 @@ function throttle(func, limit) {
     }
   };
 }
-/*
-`
-          <div class="${numCol} mt-5">
-            <div id=deal-card-${i} class="card deal-card h-100">
-              <img class="card-img-top home-deal-img" src="" alt="">
-              <div class="card-body d-flex flex-column">
-                <div class="card-text deal-time"></div>
-                <div class="d-flex justify-content-end"
-                  style="display: none;">
-                  <button type="button" class="btn upvote-btn">
-                    <span class="fas fa-angle-up"></span>
-                  </button>
-                  <span class="my-auto votes-num"></span>
-                  <button type="button" class="btn downvote-btn">
-                    <span class="fas fa-angle-down"></span>
-                  </button>
-                </div>
-                <h5 class="card-title deal-title"></h5>
-                <div>Restaurant: <a href='#' class="card-text deal-restaurant">
-                </a></div>
-                <div>Posted by: <a href='#' class="card-text deal-poster"></a>
-                </div>
-                <div class= "card-text tags" ></div>
-                <a href="#"
-                  class="btn btn-primary align-self-end mt-auto float-right">
-                  See More
-                </a>
-              </div>
-            </div>
-          </div>`*/
