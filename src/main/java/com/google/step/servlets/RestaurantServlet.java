681ca9c1196b8e87600d91906d414ff85109ec07
jyx11011 <jyx11011@gmail.com>
2020-07-23 12:01:35 +0800

    Resolve conflicts

--- src/main/java/com/google/step/servlets/RestaurantServlet.java
+++ src/main/java/com/google/step/servlets/RestaurantServlet.java
@@ -36,10 +36,7 @@ public class RestaurantServlet extends HttpServlet {
   public RestaurantServlet() {
     restaurantManager = new RestaurantManagerDatastore();
     dealManager = new DealManagerDatastore();
-<<<<<<< HEAD
     restaurantPlaceManager = new RestaurantPlaceManagerDatastore();
-=======
->>>>>>> 0e27b6a5e41b14bfc58c31928caadf1caf522b87
   }
 
   /** Deletes the restaurant with the given id parameter */
