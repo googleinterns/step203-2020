214906ec707d002cfe351a7652c44b258cb8258c
jyx11011 <jyx11011@gmail.com>
2020-07-24 18:23:47 +0800

    Fix user brief json

--- src/test/java/com/google/step/TestConstants.java
+++ src/test/java/com/google/step/TestConstants.java
@@ -16,6 +16,8 @@ public class TestConstants {
   public static final String BLOBKEY_URL_A = URL_PREFIX + BLOBKEY_A;
   public static final String BLOBKEY_URL_B = URL_PREFIX + BLOBKEY_B;
 
+  public static final String DEFAULT_PHOTO_URL = "/images/default-profile-pic.svg";
+
   // User
   public static final long USER_ID_A = 1;
   public static final long USER_ID_B = 2;
@@ -49,7 +51,8 @@ public class TestConstants {
       String.format(
           "{id: %d, username: \"%s\", picture: \"%s\"}", USER_ID_B, USERNAME_B, BLOBKEY_URL_B);
   public static final String USER_C_BRIEF_JSON =
-      String.format("{id: %d, username: \"%s\"}", USER_ID_C, USERNAME_C);
+      String.format(
+          "{id: %d, username: \"%s\", picture: \"%s\"}", USER_ID_C, USERNAME_C, DEFAULT_PHOTO_URL);
 
   // Tag
   public static final String TAG_NAME_A = "1for1";
