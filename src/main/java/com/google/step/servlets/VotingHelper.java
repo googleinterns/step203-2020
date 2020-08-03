a5a021fd8692cf6c5b2bb1757dc680a508b8c21a
Lisa <lisa.shannon.goh@gmail.com>
2020-08-03 19:24:46 +0800

    Remove comment

--- src/main/java/com/google/step/servlets/VotingHelper.java
+++ src/main/java/com/google/step/servlets/VotingHelper.java
@@ -13,7 +13,6 @@ public class VotingHelper {
       DealVoteCountManager dealVoteCountManager) {
     int prevDir = voteManager.getDirection(userId, dealId);
     int dirInt = Integer.parseInt(dir);
-    // voteManager.vote(userId, dealId, dirInt);
     if (dirInt != prevDir) {
       voteManager.vote(userId, dealId, dirInt);
       dealVoteCountManager.updateDealVotes(dealId, dirInt - prevDir);
