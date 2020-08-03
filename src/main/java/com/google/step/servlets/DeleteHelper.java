685eaec1b637289fc9d736a3e24688a84c63f55f
jyx11011 <jyx11011@gmail.com>
2020-08-03 16:54:09 +0800

    Modify delete deal

--- src/main/java/com/google/step/servlets/DeleteHelper.java
+++ src/main/java/com/google/step/servlets/DeleteHelper.java
@@ -1,7 +1,11 @@
 package com.google.step.servlets;
 
+import com.google.step.datamanager.CommentManager;
+import com.google.step.datamanager.CommentManagerDatastore;
 import com.google.step.datamanager.DealManager;
 import com.google.step.datamanager.DealManagerDatastore;
+import com.google.step.datamanager.DealTagManager;
+import com.google.step.datamanager.DealTagManagerDatastore;
 import com.google.step.datamanager.RestaurantManager;
 import com.google.step.datamanager.RestaurantManagerDatastore;
 import com.google.step.model.Deal;
@@ -10,6 +14,10 @@ import java.util.List;
 public class DeleteHelper {
   private DealManager dealManager = new DealManagerDatastore();
   private RestaurantManager restaurantManager = new RestaurantManagerDatastore();
+  private DealTagManager dealTagManager = new DealTagManagerDatastore();
+  private CommentManager commentManager = new CommentManagerDatastore();
+
+  public DeleteHelper() {}
 
   public DeleteHelper(DealManager dealManager, RestaurantManager restaurantManager) {
     this.dealManager = dealManager;
@@ -17,8 +25,11 @@ public class DeleteHelper {
   }
 
   public void deleteDeal(long id) {
+    Deal deal = dealManager.readDeal(id);
     dealManager.deleteDeal(id);
-    // Todo: delete vote,deal-tag, comments,image
+    commentManager.deleteAllCommentsOfDeal(id);
+    ImageUploader.deleteImage(deal.photoBlobkey);
+    dealTagManager.deleteAllTagsOfDeal(id);
   }
 
   public void deleteRestaurant(long id) {
